c78fb5f23a2756e35f1e83bb612f053714f621d5
Ali <work.gh.2021@gmail.com>
2025-04-08 15:04:15 +0330

    add KeyboardButton to the /start command

--- main.py
+++ main.py
@@ -4,18 +4,24 @@ import os
 from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram import KeyboardButton, ReplyKeyboardMarkup
 
 # initial settings
 load_dotenv()
 TOKEN = os.getenv('my_token_2')
 app = ApplicationBuilder().token(TOKEN).build()
+ADD_REMINDER_TEXT = 'let\'s set a reminder ⏰'
+SHOW_ALL_TEXT = '🗓 show all 🗓'
 # logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger()
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hi dear user ...")
+    keyboard_btn = [[KeyboardButton(text=ADD_REMINDER_TEXT), KeyboardButton(text=SHOW_ALL_TEXT)]]
+    markup = ReplyKeyboardMarkup(keyboard_btn, resize_keyboard=True)
+
+    await update.message.reply_text("Hi dear user ...", reply_markup=markup)
 
 
 def main():
